6c1576cd491e22fba3f2a989857844affef096a8
lzxnau <lzxnau.gmail.com>
2023-10-27 11:18:33 +1100

    update

--- src/conf.py
+++ src/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../src/pns'))
+
 # -- Project information -----------------------------------------------------
 project = 'JLDP'
 copyright = '2023, JLDP'
